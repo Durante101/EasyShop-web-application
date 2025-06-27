16dd0f27b2df711f401438ed9ff8bdbd79e8fb06
Durante101 <durthornton@gmail.com>
2025-06-26 22:47:34 -0400

    Declare DAO interface method to retrieve shopping cart by user ID

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,14 @@
 package org.yearup.data;
 
+import org.yearup.models.Category;
 import org.yearup.models.ShoppingCart;
 
-public interface ShoppingCartDao
-{
+public interface ShoppingCartDao {
     ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+
+
+
+
+
+
 }
---
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -0,0 +1,11 @@
+package org.yearup.data.mysql;
+
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.ShoppingCart;
+
+import javax.sql.DataSource;
+
+public class MySqlShoppingCartDao {
+
+
+}
